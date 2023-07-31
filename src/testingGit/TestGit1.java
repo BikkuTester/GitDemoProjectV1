e6dcc75b80cd558a6f5fc05f21bb0b5ce521e90d
ThakareSupriya <mudesupriya53@gmail.com>
2023-07-31 17:07:37 +0530

    First Commit...

---
+++ src/testingGit/TestGit1.java
@@ -0,0 +1,16 @@
+package testingGit;
+
+public class TestGit1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry = "yes";
+		
+		if(hungry.equals("yes")) {
+			System.out.println("eat something...");
+		}else
+			System.out.println("Do work...");
+	}
+
+}
