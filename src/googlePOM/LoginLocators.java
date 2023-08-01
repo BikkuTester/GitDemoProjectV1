c9e1c12ec1bed633a09f079651bbd0883a27f5e5
ThakareSupriya <mudesupriya53@gmail.com>
2023-08-01 11:09:20 +0530

    GooglePom created and login page test cases added

---
+++ src/googlePOM/LoginLocators.java
@@ -0,0 +1,11 @@
+package googlePOM;
+
+public class LoginLocators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this file will contain all the locators from google");
+	}
+
+}
